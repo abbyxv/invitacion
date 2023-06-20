bd46be5934dc15ea041c4282822d8a6e8b288148
Jose L Jr Garza Z <correojunior@gmail.com>
2023-06-20 11:32:00 -0600

    sectionid invitacion

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function IndexPage() {
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="noticias" heading="Ultimas Noticias" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="Invitacion" />
+        <AboutSection sectionId="invitacion" heading="Invitacion" />
         <InterestsSection sectionId="interests" heading="Details" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
         <ContactSection sectionId="github" heading="Issues?" />

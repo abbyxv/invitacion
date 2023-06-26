b8998c1662cc97f2b32f73db144df26a55fa7c1e
Jose L Jr Garza Z <correojunior@gmail.com>
2023-06-26 08:57:39 -0600

    whatsapp SEO info

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ import { HeroSection } from "../components/Hero";
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Invitación XV años de Abby" description="XV años de Abby, Viernes 28 de Julio, confirma asistencia al " />
+      <Seo title="Invitación al XV años de Abby" description="Viernes 28 de Julio, confirma asistencia al whatsapp +52.81.2063.5361" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <Countdown date="July 28, 2023 18:00:00 GMT-60:00" />
